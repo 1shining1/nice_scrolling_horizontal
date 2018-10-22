5b76f6164e256cdd18520db79145f3c4ddd8efb6
1shining1 <shining2327@gmail.com>
2018-10-22 14:49:57 +0900

    first commit

---
+++ js/example.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  /*
+	* Internal use of the demo website
+	*/
+  $("#showExamples").click(function(e) {
+    console.log("va");
+    e.stopPropagation();
+    e.preventDefault();
+    $("#examplesList").toggle();
+  });
+
+  $("html").click(function() {
+    $("#examplesList").hide();
+  });
+});
